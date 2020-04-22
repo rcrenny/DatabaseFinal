39354194ccf6d77762a6c57dbb910324daf5a839
acrit06 <ajcritelli6@gmail.com>
2020-04-22 14:19:03 -0400

    Update NBADatabse.sql

--- NBADatabse.sql
+++ NBADatabse.sql
@@ -23,7 +23,7 @@ CREATE TABLE PLAYER(
 	);
 
 CREATE TABLE MADE_BASKET(
-	PlayerID			Int				NOT NULL,
+	PlayerID			Char(8)			NOT NULL,
 	Time_Basket_Scored  Int             NOT NULL,
     AssistID            Int             NULL,
 	CONSTRAINT			Made_Basket_FK	FOREIGN KEY(PlayerID)
@@ -31,42 +31,42 @@ CREATE TABLE MADE_BASKET(
 	);
     
 CREATE TABLE POINTS(
-	PlayerID			Int				NOT NULL,
+	PlayerID			Char(8)			NOT NULL,
 	Made_Basket_Value   Int             NOT NULL,
 	CONSTRAINT			Made_Basket_FK	FOREIGN KEY(PlayerID)
 							REFERENCES PLAYER(PlayerID)
 	);
   
 CREATE TABLE REBOUND(
-	PlayerID			Int				NOT NULL,
+	PlayerID			Char(8)			NOT NULL,
 	Time_Rebound        Int             NOT NULL,
 	CONSTRAINT			REBOUND_FK	    FOREIGN KEY(PlayerID)
 							REFERENCES PLAYER(PlayerID)
 	);
 
 CREATE TABLE FOUL(
-	PlayerID			Int				NOT NULL,
+	PlayerID			Char(8)			NOT NULL,
 	Time_Foul           Int             NOT NULL,
 	CONSTRAINT			FOUL_FK	        FOREIGN KEY(PlayerID)
 							REFERENCES PLAYER(PlayerID)
 	);
     
 CREATE TABLE TURNOVER(
-	PlayerID			Int				NOT NULL,
+	PlayerID			Char(8)			NOT NULL,
 	Time_Turnover       Int             NOT NULL,
 	CONSTRAINT			Made_Basket_FK	FOREIGN KEY(PlayerID)
 							REFERENCES PLAYER(PlayerID)
 	);
     
 CREATE TABLE STEAL(
-	PlayerID			Int				NOT NULL,
+	PlayerID			Char(8)			NOT NULL,
 	Time_Steal          Int             NOT NULL,
 	CONSTRAINT			STEAL_FK	    FOREIGN KEY(PlayerID)
 							REFERENCES PLAYER(PlayerID)
 	);
     
 CREATE TABLE BLOCK(
-	PlayerID			Int				NOT NULL,
+	PlayerID			Char(8)			NOT NULL,
 	Time_Block          Int             NOT NULL,
 	CONSTRAINT			BLOCK_FK	    FOREIGN KEY(PlayerID)
 							REFERENCES PLAYER(PlayerID)
