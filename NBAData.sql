6513bac466e07acb31cb9f20dcea3d5158e17482
acrit06 <ajcritelli6@gmail.com>
2020-04-22 13:48:34 -0400

    Update NBAData.sql

--- NBAData.sql
+++ NBAData.sql
@@ -2,23 +2,43 @@
 INSERT INTO PLAYER VALUES
 ('00000000', 'Lauri Markkanen', 84, 240, 10, 2, 4, 4, 1, 1, 0);
 INSERT INTO PLAYER VALUES
-('00000001', 'Wendell Carter Jr.', 84, 240, 10, 2, 4, 4, 1, 1, 0);
+('00000001', 'Wendell Carter Jr.', 81, 270, 6, 2, 11, 3, 1, 3, 0);
 INSERT INTO PLAYER VALUES
-('00000002', 'Lauri Markkanen', 84, 240, 10, 2, 4, 4, 1, 1, 0);
+('00000002', 'Kris Dunn', 75, 205, 3, 3, 3, 3, 1, 1, 1);
 INSERT INTO PLAYER VALUES
-('00000003', 'Lauri Markkanen', 84, 240, 10, 2, 4, 4, 1, 1, 0);
+('00000003', 'Zach Lavine', 78, 200, 12, 2, 8, 0, 2, 1, 0);
 INSERT INTO PLAYER VALUES
-('00000004', 'Lauri Markkanen', 84, 240, 10, 2, 4, 4, 1, 1, 0);
+('00000004', 'Tomas Satoransky', 79, 210, 7, 3, 3, 3, 2, 0, 0);
 INSERT INTO PLAYER VALUES
-('00000005', 'Lauri Markkanen', 84, 240, 10, 2, 4, 4, 1, 1, 0);
+('00000005', 'Thaddeus Young', 80, 235, 10, 2, 5, 0, 1, 1, 0);
 INSERT INTO PLAYER VALUES
-('00000006', 'Lauri Markkanen', 84, 240, 10, 2, 4, 4, 1, 1, 0);
+('00000006', 'Daniel Gafford', 82, 234, 4, 0, 2, 2, 0, 0, 6);
 INSERT INTO PLAYER VALUES
-('00000007', 'Lauri Markkanen', 84, 240, 10, 2, 4, 4, 1, 1, 0);
+('00000007', 'Ryan Arcidiacono', 75, 195, 12, 1, 3, 2, 0, 1, 0);
 INSERT INTO PLAYER VALUES
-('00000008', 'Lauri Markkanen', 84, 240, 10, 2, 4, 4, 1, 1, 0);
+('00000008', 'Coby White', 76, 195, 3, 3, 3, 1, 2, 2, 0);
 INSERT INTO PLAYER VALUES
-('00000009', 'Lauri Markkanen', 84, 240, 10, 2, 4, 4, 1, 1, 0);
+('00000009', 'Denzel Valentine', 76, 220, 6, 2, 3, 2, 2, 1, 0);
+
+INSERT INTO PLAYER VALUES
+('00000010', 'PJ Washington', 79, 230, 6, 2, 3, 2, 2, 1, 0);
+INSERT INTO PLAYER VALUES
+('00000011', 'Miles Bridges', 78, 225, 6, 2, 3, 2, 2, 1, 0);
+INSERT INTO PLAYER VALUES
+('00000012', 'Bismack Biyombo', 80, 255, 6, 2, 3, 2, 2, 1, 0);
+INSERT INTO PLAYER VALUES
+('00000013', 'Terry Rozier', 73, 190, 6, 2, 3, 2, 2, 1, 0);
+INSERT INTO PLAYER VALUES
+('00000014', 'Devonte Graham', 73, 195, 6, 2, 3, 2, 2, 1, 0);
+INSERT INTO PLAYER VALUES
+('00000015', 'Michael Kidd-Gilchrist', 78, 230, 6, 2, 3, 2, 2, 1, 0);
+INSERT INTO PLAYER VALUES
+('00000016', 'Cody Martin', 77, 205, 6, 2, 3, 2, 2, 1, 0);
+INSERT INTO PLAYER VALUES
+('00000017', 'Cody Zeller', 84, 240, 6, 2, 3, 2, 2, 1, 0);
+INSERT INTO PLAYER VALUES
+('00000018', 'Malik Monk', 75, 200, 6, 2, 3, 2, 2, 1, 0);
+
 
 
 INSERT INTO OWNED_PROPERTY VALUES(seqOPID.nextVal,
